f7238913b2928757ff83de691887394398bdf98d
RkShaRkz <darksidemetatron@gmail.com>
2023-12-18 05:24:25 +0100

    Rename MainActivity to MovieListActivity
    also add a few strings

--- app/src/main/java/com/example/mymovieapplication/MovieListActivity.kt
+++ app/src/main/java/com/example/mymovieapplication/MovieListActivity.kt
@@ -3,9 +3,9 @@ package com.example.mymovieapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MovieListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_list)
     }
 }
