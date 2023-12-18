6e5ba2a1752a3996050e8d7d9782c23626557667
RkShaRkz <darksidemetatron@gmail.com>
2023-12-18 06:23:59 +0100

    Switched to the host activity idea
    We will have one MainActivity which will host either the FragmentMovieList or FragmentMovieDetails in it.
    Also, adapted activity_main to just have a toolbar and a FragmentContainerView and moved the old main activity
    contents to the new fragment_movie_list layout

--- app/src/main/java/com/example/mymovieapplication/ActivityMain.kt
+++ app/src/main/java/com/example/mymovieapplication/ActivityMain.kt
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 
-class MovieListActivity : AppCompatActivity() {
+class ActivityMain : AppCompatActivity() {
 
     private lateinit var toolbar : Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_list)
+        setContentView(R.layout.activity_main)
 
         toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
