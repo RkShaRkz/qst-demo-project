bd1957110f0a93287bf4a78bf2270e59a475d721
RkShaRkz <darksidemetatron@gmail.com>
2023-12-18 06:01:31 +0100

    Added toolbar to MovieListActivity, implemented design
    Now i'm thinking to switch up the architecture to just have one activity that hosts two fragments,
    either MovieListFragment or the MovieDetailsFragment and both fragments just interacting with the
    activity's toolbar/actionbar

--- app/src/main/java/com/example/mymovieapplication/MovieListActivity.kt
+++ app/src/main/java/com/example/mymovieapplication/MovieListActivity.kt
@@ -2,10 +2,19 @@ package com.example.mymovieapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MovieListActivity : AppCompatActivity() {
+
+    private lateinit var toolbar : Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
+
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
     }
+
+
 }
