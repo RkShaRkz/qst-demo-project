f7faa9a52784fc4f4d458779e2819eb16ba58131
RkShaRkz <darksidemetatron@gmail.com>
2023-12-18 06:37:28 +0100

    Refactor main activity (#1)
    * Rename MainActivity to MovieListActivity
    
    also add a few strings
    
    * Added toolbar to MovieListActivity, implemented design
    
    Now i'm thinking to switch up the architecture to just have one activity that hosts two fragments,
    either MovieListFragment or the MovieDetailsFragment and both fragments just interacting with the
    activity's toolbar/actionbar
    
    * Switched to the host activity idea
    
    We will have one MainActivity which will host either the FragmentMovieList or FragmentMovieDetails in it.
    Also, adapted activity_main to just have a toolbar and a FragmentContainerView and moved the old main activity
    contents to the new fragment_movie_list layout

--- app/src/main/java/com/example/mymovieapplication/ActivityMain.kt
+++ app/src/main/java/com/example/mymovieapplication/ActivityMain.kt
@@ -2,10 +2,19 @@ package com.example.mymovieapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : AppCompatActivity() {
+class ActivityMain : AppCompatActivity() {
+
+    private lateinit var toolbar : Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
     }
+
+
 }
